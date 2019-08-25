9f9e71e9cbc197ed519db8335bc01c6eebbde23d
vmejazz <vmejazz@gmail.com>
2019-08-25 15:21:55 +0300

    Немного адаптив для мобилки

---
+++ source/js/modal.js
@@ -0,0 +1,36 @@
+(function () {
+  var modalCallback = document.querySelector('.modal-callback');
+  var modalOverlay = modalCallback.querySelector('.modal-callback__overlay');
+  var mainPromotionButton = document.querySelector('.main-promotion__button');
+  var modalCallbackCloseButton = modalCallback.querySelector('.modal-callback__button--close');
+
+  var modalCallbackOpenButton = document.querySelector('.callback__button');
+
+  var closeModal = function (evt) {
+    modalCallback.classList.remove('modal-callback--show');
+    modalCallback.classList.add('modal-callback--hide');
+  }
+
+
+  var openModal = function (evt) {
+    evt.preventDefault()
+    modalCallback.classList.add('modal-callback--show');
+    modalCallback.classList.remove('modal-callback--hide');
+    document.addEventListener('keydown', function(evt){
+      if (evt.keyCode === 27) {
+        closeModal();
+      }
+    })
+    // modalOverlay.addEventListener('click', closeModal,false);
+  };
+
+
+
+  modalCallbackOpenButton.addEventListener('click', openModal);
+  modalCallbackCloseButton.addEventListener('click', closeModal);
+  mainPromotionButton.addEventListener('click',openModal);
+
+  document.addEventListener('keydown', function(evt){
+    console.log(evt.keyCode);
+  })
+  })();
